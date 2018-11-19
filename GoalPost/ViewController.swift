b88c66c412c73685e0d7331ea6b261b31b71ae84
Milan Bojic <milan.bojic.developer@gmail.com>
2018-11-19 14:04:29 +0100

    Initial Commit

---
+++ GoalPost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoalPost
+//
+//  Created by Milan Bojic on 11/19/18.
+//  Copyright © 2018 Milan Bojic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
